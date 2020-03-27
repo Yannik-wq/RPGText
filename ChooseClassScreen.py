941de898615f1bfdd37b36a60e76db216b322d38
Yannik-wq <62504730+Yannik-wq@users.noreply.github.com>
2020-03-27 14:52:29 +0100

    Add files via upload
    Same display setup but using the classes Rectangle und TextBox(Rectangle)

--- ChooseClassScreen.py
+++ ChooseClassScreen.py
@@ -1,46 +1,64 @@
-def ChooseClass():
-    import pygame, CaptRectcreate, OpenStartScreen
-    pygame.init()
-    pygame.font.init()
-    pygame.display.init()                                                       
-    screen = pygame.display.set_mode(size = (1300,1000))
+import pygame, Class_Rect, OpenStartScreen, InfoFighter
 
 
-        
-    FighterRect = CaptRectcreate.CaptRect((600,200),(150,50),'Fighter')
-    
-    RogueRect = CaptRectcreate.CaptRect((400,200),(150,50),'Rogue')
+
+def choose_class(screen):
+
+    #zscreen = pygame.display.set_mode(size = (1300,1000))
+    #screen = pygame.display.set_mode(size = (1300,1000))
+    #pygame.display.flip()
 
 
-    MageRect = CaptRectcreate.CaptRect((800,200),(150,50),'Mage')
-    
+    #fighter_rect = CaptRectcreate.capt_rect((600,200),(150,50),'Fighter',50)
+    fighter =  Class_Rect.TextBox(screen,(600,200),(150,50),50,'Fighter')
+    fighter_rect = fighter.give_rect()
+    fighter.draw_button()
 
-    ExitRect = CaptRectcreate.CaptRect((100,600),(150,50),'Exit')
+    #rogue_rect = CaptRectcreate.capt_rect((400,200),(150,50),'Rogue',50)
+    rogue =  Class_Rect.TextBox(screen,(400,200),(150,50),50,'Rogue')
+    rogue_rect = rogue.give_rect()
+    rogue.draw_button()
 
-    
-    ZurRect = CaptRectcreate.CaptRect((100,400),(150,50),'Zurück')
-    
+    #mage_rect = CaptRectcreate.capt_rect((800,200),(150,50),'Mage',50)
+    mage =  Class_Rect.TextBox(screen,(800,200),(150,50),50,'Mage')
+    mage_rect = mage.give_rect()
+    mage.draw_button()
+
+    #exit_rect = CaptRectcreate.capt_rect((100,600),(150,50),'Exit',50)
+    exit =  Class_Rect.TextBox(screen,(100,600),(100,50),50,'Exit')
+    exit_rect = exit.give_rect()
+    exit.draw_button()
+
+    #zur_rect = CaptRectcreate.capt_rect((300,600),(150,50),'Zurück',50)
+    zur =  Class_Rect.TextBox(screen,(300,600),(150,50),50,'Zurück')
+    zur_rect = zur.give_rect()
+    zur.draw_button()
 
     a = 0
-    pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)                            
+    pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)
     while a == 0:
-        for event in pygame.event.get():                                        
-            if event.type == pygame.MOUSEBUTTONDOWN:                            
-                if FighterRect.collidepoint(pygame.mouse.get_pos()):              
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if fighter_rect.collidepoint(pygame.mouse.get_pos()):
                     a = 0
-                    #InfoFighter()                                                      
-                elif RogueRect.collidepoint(pygame.mouse.get_pos()):
+                    fighter.delete()
+                    rogue.delete()
+                    mage.delete()
+                    InfoFighter.info_fighter(screen,exit,zur)
+                elif rogue_rect.collidepoint(pygame.mouse.get_pos()):
                     a = 0
                     #InfoRogue()
-                elif MageRect.collidepoint(pygame.mouse.get_pos()):
+                elif mage_rect.collidepoint(pygame.mouse.get_pos()):
                     a = 0
                     #InfoMage()
-                elif ExitRect.collidepoint(pygame.mouse.get_pos()):              
+                elif exit_rect.collidepoint(pygame.mouse.get_pos()):
                     a = 1
                     pygame.display.quit()
-                elif ZurRect.collidepoint(pygame.mouse.get_pos()):
-                    pygame.display.flip()
-                    OpenStartScreen.OpenStartScreen()
-                    break
-            else:
-                continue
+                elif zur_rect.collidepoint(pygame.mouse.get_pos()):
+                    #pygame.display.flip()
+                    a = 1
+                    zur.delete()
+                    fighter.delete()
+                    rogue.delete()
+                    mage.delete()
+                    OpenStartScreen.open_start_screen()
--- OpenStartScreen.py
+++ OpenStartScreen.py
@@ -1,35 +1,31 @@
-def OpenStartScreen():
-    import pygame, ChooseClassScreen,CaptRectcreate
-    pygame.init()
-    pygame.font.init()
-    pygame.display.init()                                                       #Initialisiert das display module
+import pygame, ChooseClassScreen, Class_Rect
 
+def open_start_screen():
+                                                         #Initialisiert das display module
 
+    start_screen = pygame.display.set_mode(size = (1300,800))                        #Initilisiert die display surface
 
-    pygame.display.set_caption('Rats Everywhere')
-    StartScreen = pygame.display.set_mode(size = (1300,800))                        #Initilisiert die display surface
-                                 
-    ExitRect = CaptRectcreate.CaptRect((100,600),(150,50),'Exit')
-   
-    StartRect = CaptRectcreate.CaptRect((100,400),(100,50),'Start')
-    
+    exit =  Class_Rect.TextBox(start_screen,(100,600),(100,50),50,'Exit')
+    exit_rect = exit.give_rect()
+    exit.draw_button()
+    #CaptRectcreate.capt_rect((100,600),(150,50),'Exit',50)
+
+    start = Class_Rect.TextBox(start_screen,(100,400),(100,50),50,'Start')
+    #CaptRectcreate.capt_rect((100,400),(100,50),'Start',50)
+    start_rect = start.give_rect()
+    start.draw_button()
+
     a = 0
     pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)                            #Only mousebutton presses are filled into the event queue, everything else is droped.
     while a == 0:
-        for event in pygame.event.get():                                        #grabs all events from the event que      
+        for event in pygame.event.get():                                        #grabs all events from the event que
             if event.type == pygame.MOUSEBUTTONDOWN:                            #Checks if these events are Mousebuttonpresses
-                if ExitRect.collidepoint(pygame.mouse.get_pos()):              #Checks wether position of the mouse when clicked is within the 'Firstrect' rectangle
+                if exit_rect.collidepoint(pygame.mouse.get_pos()):              #Checks wether position of the mouse when clicked is within the 'Firstrect' rectangle
                     a = 2
                     pygame.display.quit()
-                    break                                                   #closes entire display module
-                
-                elif StartRect.collidepoint(pygame.mouse.get_pos()):
+
+                elif start_rect.collidepoint(pygame.mouse.get_pos()):
                     a = 1
-                    StartButDraw = pygame.draw.rect(StartScreen, (0,0,0), StartRect)
-                    ChooseClassScreen.ChooseClass()
-            else:
-                continue
-
-
-        
-    
+                    start.delete()
+                    #StartButDraw = pygame.draw.rect(start_screen, (0,0,0), start_rect)
+                    ChooseClassScreen.choose_class(start_screen)
