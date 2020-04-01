9d8eff07770051cb4d60c6ee0992fe73092c534e
CoronaSurvivor <leander.rolef@gmail.com>
2020-04-01 02:03:47 +0200

    some work on the class system

--- classes.py
+++ classes.py
@@ -12,34 +12,49 @@ class CharacterClass:
         self.base_arcane_resistance = base_arcane_resistance
 
 
-fighter = CharacterClass(
-    base_strength = 40,
-    base_intelligence = 30,
-    base_dexterity = 35,
-    base_vitality = 40,
-    base_luck = 30,
-    base_resilience = 40,
-    base_arcane_resistance = 35
+class PlayerCharacter(CharacterClass):
+    def __init__(self, player_class, skillpoint_allocation):
+        pass
+    def update_attributes(): #gearchange, combat, etc.
+        pass
+    def update_other(): #maybe social standing, knowledge, etc. due to quests
+        pass
+
+class Fighter(CharacterClass):
+    def __init__(self):
+        super().__init__(
+                        base_strength = 40,
+                        base_intelligence = 30,
+                        base_dexterity = 35,
+                        base_vitality = 40,
+                        base_luck = 30,
+                        base_resilience = 40,
+                        base_arcane_resistance = 35
                         )
 
-mage = CharacterClass(
-    base_strength = 30,
-    base_intelligence = 50,
-    base_dexterity = 35,
-    base_vitality = 30,
-    base_luck = 30,
-    base_resilience = 30,
-    base_arcane_resistance = 45
+
+class Mage(CharacterClass):
+    def __init__(self):
+        super().__init__(
+                        base_strength = 30,
+                        base_intelligence = 50,
+                        base_dexterity = 35,
+                        base_vitality = 30,
+                        base_luck = 30,
+                        base_resilience = 30,
+                        base_arcane_resistance = 45
                         )
 
-rogue = CharacterClass(
-    base_strength = 35,
-    base_intelligence = 35,
-    base_dexterity = 45,
-    base_vitality = 35,
-    base_luck = 35,
-    base_resilience = 30,
-    base_arcane_resistance = 35
+class Rogue(CharacterClass):
+    def __init__(self):
+        super().__init__(
+                        base_strength = 35,
+                        base_intelligence = 35,
+                        base_dexterity = 45,
+                        base_vitality = 35,
+                        base_luck = 35,
+                        base_resilience = 30,
+                        base_arcane_resistance = 35
                         )
-
-print(rogue.base_luck)
+jack = Rogue()
+print(jack.base_luck)
