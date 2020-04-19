8e75736ffecfc76917a4568f5e5772527922bbfa
Yannik-wq <62504730+Yannik-wq@users.noreply.github.com>
2020-04-20 00:09:26 +0200

    Add files via upload

--- OpenStartScreen.py
+++ OpenStartScreen.py
@@ -1,31 +1,26 @@
-import pygame, ChooseClassScreen, Class_Rect
+import pygame
+import sys
+import Class_Rect
+import ChooseClassScreen
 
-def open_start_screen():
+def open_start_screen(screen, exit):
                                                          #Initialisiert das display module
-
-    start_screen = pygame.display.set_mode(size = (1300,800))                        #Initilisiert die display surface
-
-    exit =  Class_Rect.TextBox(start_screen,(100,600),(100,50),50,'Exit')
-    exit_rect = exit.give_rect()
     exit.draw_button()
-    #CaptRectcreate.capt_rect((100,600),(150,50),'Exit',50)
-
-    start = Class_Rect.TextBox(start_screen,(100,400),(100,50),50,'Start')
-    #CaptRectcreate.capt_rect((100,400),(100,50),'Start',50)
-    start_rect = start.give_rect()
+    start = Class_Rect.TextBox(screen,(100,400),(100,50),50,'Start')
     start.draw_button()
 
     a = 0
     pygame.event.set_allowed(pygame.MOUSEBUTTONDOWN)                            #Only mousebutton presses are filled into the event queue, everything else is droped.
     while a == 0:
-        for event in pygame.event.get():                                        #grabs all events from the event que
+        for event in pygame.event.get():                                        #grabs all events from the event queue
             if event.type == pygame.MOUSEBUTTONDOWN:                            #Checks if these events are Mousebuttonpresses
-                if exit_rect.collidepoint(pygame.mouse.get_pos()):              #Checks wether position of the mouse when clicked is within the 'Firstrect' rectangle
+                if exit.check_press(event):              #Checks wether position of the mouse when clicked is within the 'Firstrect' rectangle
                     a = 2
                     pygame.display.quit()
-
-                elif start_rect.collidepoint(pygame.mouse.get_pos()):
+                    sys.exit(0)
+                elif start.check_press(event):
                     a = 1
                     start.delete()
-                    #StartButDraw = pygame.draw.rect(start_screen, (0,0,0), start_rect)
-                    ChooseClassScreen.choose_class(start_screen)
+
+    id = ChooseClassScreen.choose_class(screen, exit)
+    return (id)
